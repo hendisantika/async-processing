4bf929c40baa3c1e341cd571c90f7c4b29ba0887
Hendi Santika <hendisantika@gmail.com>
2017-05-11 21:05:42 +0700

    #2 - Bug Fixed TaskServiceImpl.java

--- src/main/java/com/hendisantika/async/service/TaskServiceImpl.java
+++ src/main/java/com/hendisantika/async/service/TaskServiceImpl.java
@@ -2,10 +2,12 @@ package com.hendisantika.async.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by hendisantika on 5/11/17.
  */
+@Service
 public class TaskServiceImpl implements TaskService {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
